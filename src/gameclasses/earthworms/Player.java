a4fc77f350cfafb4e8b2c77f4b16732c6f90ae88
pancirno <pancirno@LenovoDebian.home>
2015-09-20 15:52:08 +0200

    the ball moves

--- src/gameclasses/earthworms/Player.java
+++ src/gameclasses/earthworms/Player.java
@@ -17,15 +17,21 @@ import wormgame.*;
  */
 public class Player
 {
-    int x;
+    int x = 500;
     int y;
     
     boolean shouldExplode = false;
     
+    //user movement
     boolean moveLeft = false;
     boolean moveRight = false;
+    boolean isFalling = false;
+    boolean faceDirection = false;
     
-    boolean overlapsLevel = false;
+    //physics
+    boolean freeFall = false;
+    double vx = 0;
+    double vy = 0;
     
     public void step(GSGame gs)
     {
@@ -35,21 +41,25 @@ public class Player
             gs.spawnExplosion(ExplosionFactory.MakeMediumExplosion(x,y));
         }
         
-        if(moveLeft)
+        if(!freeFall) //handle by player movement
         {
-            x-=5;
+            if(moveLeft)
+            {
+                doWalking(gs, false, 1);
+            }
+            if(moveRight)
+            {
+                doWalking(gs, true, 1);
+            }
+        
+            doFalling(gs);
+        }
+        else //free fall
+        {
+            
         }
         
-        if(moveRight)
-        {
-            x+=5;
-        }
         
-        if(gs.currentStage.Collide(x, y))
-        {
-            overlapsLevel = true;
-        }
-        else overlapsLevel = false;
         
     }
     
@@ -60,11 +70,8 @@ public class Player
         
         int anchx = c.GetCameraDeltaX(x);
         int anchy = c.GetCameraDeltaY(y);
-        
-        
-        
-        if(overlapsLevel)loop.GetGraphicsContext().setFill(Color.BLANCHEDALMOND);
-        else loop.GetGraphicsContext().setFill(Color.RED);
+                
+        loop.GetGraphicsContext().setFill(Color.RED);
             
         loop.GetGraphicsContext().fillOval(anchx-12, anchy-12, 24, 24);
     }
@@ -86,15 +93,91 @@ public class Player
         }
         if(ie.keyStatus(KeyCode.LEFT) == true)
         {
+            faceDirection = false;
             moveLeft = true;
         }
         if(ie.keyStatus(KeyCode.RIGHT) == true)
         {
+            faceDirection = true;
             moveRight = true;
         }
         if(ie.keyStatus(KeyCode.Z) == true)
         {
             shouldExplode = true;
+        }
+    }
+    
+    void doWalking(GSGame gs, boolean dir, int speed)
+    {
+        if(isFalling) return;
+        
+        int sign = 1;
+        if(dir == false) sign = -1;
+        
+        while(speed > 0)
+            {
+                if(gs.currentStage.Collide(x  + (speed * sign), y))
+                {
+                    speed--;
+                }
+                else
+                {
+                    x += speed * sign;
+                    return;
+                }
+                
+                if(speed == 0)
+                {
+                    for(int climb = 1; climb <= 8; climb++)
+                    {
+                        if(!gs.currentStage.Collide(x, y-climb))
+                        {
+                            if(!gs.currentStage.Collide(x + 1 * sign , y-climb));
+                            {
+                                y = y-climb;
+                                x = x+1*sign;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    void doFalling(GSGame gs)
+    {
+        if(isFalling)
+        {
+            if(gs.currentStage.Collide(x, y+1))
+            {
+                isFalling = false;
+                return;
+            }
+            else
+            {
+                y+=1;
+                return;
+            }
+        }
+        
+        if(gs.currentStage.Collide(x, y+1))
+        {
+            isFalling = false;
+        }
+        else
+        {
+            for(int fall = 2; fall <= 8; fall++)
+            {
+                if(gs.currentStage.Collide(x, y+fall))
+                {
+                    y = y+fall-1;
+                    isFalling = false;
+                    return;
+                }
+            }
+            
+            isFalling = true;
+            y+=1;
         }
     }
 }
